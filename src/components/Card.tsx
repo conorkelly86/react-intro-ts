db24a0f73374b3b051a904e7c0246d475c0dcad8
Conor Kelly <conor.kelly.irishlife@gmail.com>
2022-02-10 17:21:22 +0000

    Removing sort

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,36 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
 import players from "../players.json";
 
 export interface CardProps {
   name: string;
-  position: string;
-  image: string;
 }
 
-export const Card: React.FC<CardProps> = ({}) => {
-  const [order, setOrder] = useState(players);
-
-  const sortByAge = () => {
-    const sorted = [...order].sort((a, b) => {
-      return a.number - b.number;
-    });
-    setOrder(sorted);
-  };
-
+export const Card: React.FC<CardProps> = ({ name }) => {
   return (
     <>
-      <div className="block">
-        <div className="inline-flex mt-10">
-          <button
-            onClick={sortByAge}
-            className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-auto justify-center w-100"
-          >
-            Sort squad by shirt number!
-          </button>
-        </div>
+      <div className="block text-center text-xl">
+        {name}
         <div className="flex flex-wrap justify-center" style={{ width: "890px" }}>
-          {order.map((player) => (
+          {players.map((player) => (
             <div className="flex justify-center md:w-1/3 sm:1/12 p-5">
               <div className="inline-block text-center">
                 {player.image ? (
