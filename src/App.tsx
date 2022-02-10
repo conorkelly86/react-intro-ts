cd74e53aedeb803c277f0e60411e27cbe58cdd69
Conor Kelly <conor.kelly.irishlife@gmail.com>
2022-02-10 20:39:20 +0000

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export interface AppProps {
 export const App: React.FC<AppProps> = ({ card }) => {
   return (
     <div className="bg-pl-grey flex justify-center">
-      <Card name="Liverpool FC" />
+      <Card name="Liverpool F.C" />
     </div>
   );
 };
